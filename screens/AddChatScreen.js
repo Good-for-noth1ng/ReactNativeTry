a8c728d105554e1ee717b8f5a5a3354b6a2919d6
Good-for-noth1ng <apasnost@list.ru>
2022-06-24 23:17:49 +0200

    fix

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -4,6 +4,7 @@ import { Button, Input } from 'react-native-elements';
 import Icon from "react-native-vector-icons/FontAwesome"
 import { db } from '../firebase';
 
+
 const AddChatScreen = ({ navigation }) => {
     const [input, setInput] = useState('')
 
